443597e8173a31cfe2d81cf78933308a2c39511a
RumbleKAT <reki318@naver.com>
2021-10-30 13:21:40 +0900

    [feat] create event

--- week2/pages/api/events/index.ts
+++ week2/pages/api/events/index.ts
@@ -1,3 +1,8 @@
+import { IAddEventReq } from '@/controllers/event/interface/IAddEventReq';
+import { JSCAddEvent } from '@/controllers/event/jsc/JSCAddEvent';
+import FirebaseAdmin from '@/models/commons/firebase_admin.model';
+import validateParamWithData from '@/models/commons/req_validator';
+import { IEvent } from '@/models/interface/IEvent';
 import { NextApiRequest, NextApiResponse } from 'next';
 import debug from '../../../utils/debug_log';
 
@@ -8,5 +13,42 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse):
   // eslint-disable-next-line no-console
   const { method } = req;
   log(method);
-  res.status(400).send('bad request');
+
+  const validateReq = validateParamWithData<IAddEventReq>(
+    {
+      body : req.body
+    },
+    JSCAddEvent,
+  );
+
+  if (validateReq.result === false) {
+    return res.status(400).json({
+      text: validateReq.errorMessage,
+    });
+  }
+
+  const {
+    title,
+    desc,
+    owner: { uid, displayName },
+    lastOrder,
+  } = validateReq.data.body;
+
+  const addData: Omit<IEvent, 'id'> = {
+    title,
+    desc: desc ?? '',
+    ownerId: uid,
+    ownerName: displayName ?? '',
+    closed: false,
+  };
+  if (lastOrder !== undefined) {
+    addData.lastOrder = lastOrder;
+  }
+  const result = await FirebaseAdmin.getInstance().Firestore.collection('events').add(addData);
+
+  const returnValue = {
+    ...addData,
+    id: result.id,
+  };
+  res.json(returnValue);
 }
