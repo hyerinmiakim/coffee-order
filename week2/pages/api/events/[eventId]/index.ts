fd446923f6eb6662b1261581d80669fcae8ff18b
sharveka <sharveka@gmail.com>
2021-11-01 14:45:36 +0900

    complete week2 quest

--- week2/pages/api/events/[eventId]/index.ts
+++ week2/pages/api/events/[eventId]/index.ts
@@ -1,5 +1,9 @@
+/* eslint-disable prettier/prettier */
+
+import { IAddEventReq } from '@/controllers/event/interface/IAddEventReq';
 import { IFindEventReq } from '@/controllers/event/interface/IFindEventReq';
 import { IUpdateEventReq } from '@/controllers/event/interface/IUpdateEventReq';
+import { JSCAddEvent } from '@/controllers/event/jsc/JSCAddEvent';
 import { JSCFindEvent } from '@/controllers/event/jsc/JSCFindEvent';
 import { JSCUpdateEvent } from '@/controllers/event/jsc/JSCUpdateEvent';
 import FirebaseAdmin from '@/models/commons/firebase_admin.model';
@@ -10,87 +14,111 @@ import debug from '../../../../utils/debug_log';
 
 const log = debug('masa:api:events:[eventId]:index');
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  // eslint-disable-next-line no-console
-  const { method, query } = req;
-  log(method);
-
-  const supportMethod = ['PUT', 'GET'];
-  if (supportMethod.indexOf(method!) === -1) {
-    return res.status(400).end();
+// get firestore document reference
+async function getFireStoreDoc(eventId: string, res:NextApiResponse){
+  const ref = FirebaseAdmin.getInstance().Firestore.collection('events').doc(eventId);
+  const doc = await ref.get();
+  if (doc.exists === false) {
+    res.status(404).end('There is no document');
   }
-  if (method === 'GET') {
-    // 검증
-    const validateReq = validateParamWithData<IFindEventReq>(
+  return {ref, doc};
+}
+
+// Ajv validator
+export function validateReqData(method: string, req: NextApiRequest, res: NextApiResponse):any {
+  let validateReq :any;
+  if(method==='GET'){
+    validateReq = validateParamWithData<IFindEventReq>(
       {
-        params: req.query as any,
+        params: req.query as any, //eventId
       },
       JSCFindEvent,
     );
-    if (validateReq.result === false) {
-      return res.status(400).json({
-        text: validateReq.errorMessage,
-      });
-    }
-    log(`validateReq.result: ${validateReq.result}`);
-
-    // 데이터 조작(READ)
-    const ref = FirebaseAdmin.getInstance().Firestore.collection('events').doc(validateReq.data.params.eventId);
-    const doc = await ref.get();
-    // 문서가 존재하지않는가?
-    if (doc.exists === false) {
-      res.status(404).end('문서가 없어요');
-      return;
-    }
-    const returnValue = {
-      ...doc.data(),
-      id: validateReq.data.params.eventId,
-    };
-    res.json(returnValue);
+  }
+  else if(method==='PUT'){
+    validateReq = validateParamWithData<IUpdateEventReq>( //param:any, schema:JSONSchema6
+      {
+        params: req.query as any, //eventId
+        body: req.body,           //수정 사항
+      }, 
+      JSCUpdateEvent, 
+    );
+  }
+  else if(method==='POST'){
+    validateReq = validateParamWithData<IAddEventReq>(
+      {
+        body: req.body,
+      },
+      JSCAddEvent,
+    );
   }
 
-  // PUT 메서드 처리
-  if (method === 'PUT') {
-    // 코드 복사 후 IUpdateEventReq, JSCUpdateEvent 2가지는 import 시켜야합니다.
-    const token = req.headers.authorization;
+  if (validateReq.result === false) {
+    return res.status(400).json({
+      text: validateReq.errorMessage,
+    });
+  }
+  return validateReq;
+}
+
+
+// Authorization
+export async function checkUserToken(req:NextApiRequest, res:NextApiResponse): Promise<string | void>{
+  const token = req.headers.authorization;
     if (token === undefined) {
-      return res.status(400).end('token?');
+      return res.status(400).end('There is no token');
     }
     let userId = '';
     try {
+      // 유효한 토큰인지 확인
       const decodedIdToken = await FirebaseAdmin.getInstance().Auth.verifyIdToken(token);
       userId = decodedIdToken.uid;
-    } catch (err) {
-      return res.status(400).end('이상한 토큰');
+      
+    } 
+    catch (err) { // 올바르지 않으면 뱉어내기
+      return res.status(400).end('invalid token');
     }
+    return userId;
+}
 
-    const validateReq = validateParamWithData<IUpdateEventReq>(
-      {
-        params: req.query as any,
-        body: req.body,
-      },
-      JSCUpdateEvent,
-    );
-    log(req.body);
-    if (validateReq.result === false) {
-      return res.status(400).json({
-        text: validateReq.errorMessage,
-      });
-    }
-    const ref = FirebaseAdmin.getInstance().Firestore.collection('events').doc(validateReq.data.params.eventId);
-    const doc = await ref.get();
-    // 문서가 존재하지않는가?
-    if (doc.exists === false) {
-      res.status(404).end();
-    }
+// Main Function
+export default async function handle(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  // eslint-disable-next-line no-console
+  const { method } = req;
+  log(method);
+
+  const supportMethod = ['PUT', 'GET'];
+  if (supportMethod.indexOf(method!) === -1) {  //supportMethod 에 포함되지 않은 method는 400
+    return res.status(400).end('Your request is not supported');
+  }
+
+  if(method==='GET'){
+    const validateReq = validateReqData(method, req, res);
+    const {doc} = await getFireStoreDoc(validateReq.data.params.eventId, res); 
+
+    // id와 함께 문서 정보를 반환한다.
+    const returnValue = {
+      ...doc.data(),
+      id: validateReq.data.params.eventId,  
+    };
+    res.json(returnValue); 
+
+  }
+
+ 
+  else if (method === 'PUT') {
+    const userId = await checkUserToken(req,res);
+    const validateReq = validateReqData(method, req, res);
+    const {ref, doc} = await getFireStoreDoc(validateReq.data.params.eventId, res);
+
+    // userId와 비교 (인가 처리)
     const eventInfo = doc.data() as IEvent; // doc.data() 결과를 IEvent 인터페이스로 캐스팅
-    if (eventInfo.ownerId !== userId) {
-      return res.status(401).json({
-        text: '이벤트 수정 권한이 없습니다',
-      });
-    }
+      if (eventInfo.ownerId !== userId) { //eventInfo.ownerId : 문서를 만든 사람 | userId : token으로 구별하는 사람
+        return res.status(401).json({
+          text: "You're not authorized to access this event",
+        });
+      }
 
-    // eventInfo.closed = validateReq.data.body.closed ?? false;
     // 업데이트할 값
     const updateData = {
       ...eventInfo,
--- week2/pages/api/events/index.ts
+++ week2/pages/api/events/index.ts
@@ -1,5 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+// import { IAddEventReq } from '@/controllers/event/interface/IAddEventReq';
+// import { JSCAddEvent } from '@/controllers/event/jsc/JSCAddEvent';
+import FirebaseAdmin from '@/models/commons/firebase_admin.model';
+// import validateParamWithData from '@/models/commons/req_validator';
+import { IEvent } from '@/models/interface/IEvent';
 import { NextApiRequest, NextApiResponse } from 'next';
 import debug from '../../../utils/debug_log';
+import {validateReqData, checkUserToken} from './[eventId]/index';
 
 const log = debug('masa:api:events:index');
 
@@ -8,5 +16,36 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse):
   // eslint-disable-next-line no-console
   const { method } = req;
   log(method);
-  res.status(400).send('bad request');
+
+  // Authorization
+  checkUserToken(req,res);
+
+  //Ajv validator
+  const validateReq=validateReqData(method!, req, res);
+
+  // 이벤트 생성
+  const {
+    title,
+    desc,
+    owner: { uid, displayName },
+    lastOrder,
+  } = validateReq.data.body;
+  const addData: Omit<IEvent, 'id'> = {
+    title,
+    desc: desc ?? '',
+    ownerId: uid,
+    ownerName: displayName ?? '',
+    closed: false,
+  };
+  if (lastOrder !== undefined) {
+    addData.lastOrder = lastOrder;
+  }
+  const result = await FirebaseAdmin.getInstance().Firestore.collection('events').add(addData);
+
+  // 결과 반환
+  const returnValue = {
+    ...addData,
+    id: result.id,
+  };
+  res.json(returnValue);
 }
