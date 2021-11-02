e70094e4c2b5bc6d9959d82d7cb0b57722cb05c2
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-11-02 23:20:25 +0900

    create custom exception handler

---
+++ week2/controllers/util/exception/CustomExceptionHandler.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { HttpStatus } from './HttpStatus';
+
+export class CustomExceptionHandler extends Error {
+    constructor(public code: HttpStatus, ...params: ConstructorParameters<typeof Error>) {
+        super(...params);
+    
+        if (Error.captureStackTrace) {
+          Error.captureStackTrace(this, CustomExceptionHandler);
+        }
+        this.name = 'HttpError';
+      }
+} 
---
+++ week2/controllers/util/exception/HttpStatus.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+export enum HttpStatus {
+    BadRequest = 400,
+    Unauthorized = 401,
+    NotFound = 404,
+    InternalServerError = 500,
+}
