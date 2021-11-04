8145e94a432af450fb518344d42908f316e7a5ed
totuworld <totuworld@gmail.com>
2021-11-04 19:37:56 +0900

    3주차 내용 추가

---
+++ week3/pages/api/events/[eventId]/orders/[guestId].ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import debug from '../../../../../utils/debug_log';
+
+const log = debug('masa:api:events:[eventId]:orders:[guestId]');
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  // eslint-disable-next-line no-console
+  const { method } = req;
+  log(method);
+  res.status(400).send('bad request');
+}
