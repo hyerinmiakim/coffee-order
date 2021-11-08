5f4064cdf678f4c0564bb6b884346367c595b142
Chinsan Chung <chinsanchung@gmail.com>
2021-11-09 08:52:25 +0900

    오직 DELETE 요청만 받도록 조건문 추가
    - DELETE 요청 이외에는 상태코드 400을 리턴하도록 조건문을 작성했습니다.

--- week3/pages/api/events/[eventId]/orders/[guestId].ts
+++ week3/pages/api/events/[eventId]/orders/[guestId].ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import EventController from '../../../../../controllers/event/event.controller';
 import debug from '../../../../../utils/debug_log';
 
 const log = debug('masa:api:events:[eventId]:orders:[guestId]');
@@ -7,5 +8,10 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse):
   // eslint-disable-next-line no-console
   const { method } = req;
   log(method);
-  res.status(400).send('bad request');
+
+  const supportMethod = ['DELETE'];
+  if (supportMethod.indexOf(method!) === -1) {
+    return res.status(400).end();
+  }
+  await EventController.deleteOrder(req, res);
 }
