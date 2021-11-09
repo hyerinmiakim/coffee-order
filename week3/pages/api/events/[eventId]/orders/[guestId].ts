0bc96d16a630ac22ae13c05557f052b15ee096dd
Lee-ji-soo <nbsp3@naver.com>
2021-11-10 01:41:01 +0900

    Connect [order] page router

--- week3/pages/api/events/[eventId]/orders/[guestId].ts
+++ week3/pages/api/events/[eventId]/orders/[guestId].ts
@@ -1,3 +1,4 @@
+import EventController from '@/controllers/event/event.controller';
 import { NextApiRequest, NextApiResponse } from 'next';
 import debug from '../../../../../utils/debug_log';
 
@@ -7,5 +8,11 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse):
   // eslint-disable-next-line no-console
   const { method } = req;
   log(method);
-  res.status(400).send('bad request');
+  if (method !== 'DELETE') {
+    return res.status(500).end();
+  }
+  if (method === 'DELETE') {
+    console.log(method);
+    await EventController.deleteOrder(req, res);
+  }
 }
