f32400fa9a91a5d13121c01a21b14a18e86d00f0
JJ-HH <docessonhos@naver.com>
2021-11-16 15:31:26 +0900

    난이도: 하

--- week4/controllers/menuList/menu_list.controller.ts
+++ week4/controllers/menuList/menu_list.controller.ts
@@ -6,17 +6,97 @@ import FirebaseAdmin from '../../models/commons/firebase_admin.model';
 import validateParamWithData from '../../models/commons/req_validator';
 import { JSCAddMenuList } from './jsc/JSCAddMenuList';
 import { JSCUpdateMenuList } from './jsc/JSCUpdateMenuList';
+import debug from '@/utils/debug_log';
+
+const log = debug('massa:controller:menu_list');
 
 export default class MenuListController {
   static async findAllByOwnerId(req: Request, res: Response) {
-    res.status(200).json([]);
+    const token = req.headers.authorization;
+    if (token === undefined) {
+      return res.status(400).end();
+    }
+    const { uid } = await this.getUId(token);
+    log(req.body);
+    try {
+      const result = await MenuListModel.findAllByOwnerId({ ownerId: uid });
+      return res.json(result);
+    } catch (err: any) {
+      return res.status(500).send(err.toString());
+    }
   }
 
   static async addMenuList(req: Request, res: Response) {
-    res.status(400).end();
+    const token = req.headers.authorization;
+    if (token === undefined) {
+      return res.status(400).end();
+    }
+    const { uid } = await this.getUId(token);
+
+    const validateReq = validateParamWithData<{ body: Pick<IMenuListItem, 'title' | 'desc' | 'menu'> }>(
+      {
+        body: req.body,
+      },
+      JSCAddMenuList,
+    );
+
+    if (validateReq.result === false) {
+      return res.status(400).send({
+        text: validateReq.errorMessage,
+      });
+    }
+    try {
+      const result = await MenuListModel.add({
+        title: validateReq.data.body.title,
+        desc: validateReq.data.body.desc,
+        ownerId: uid,
+        menu: validateReq.data.body.menu,
+      });
+      return res.json(result);
+    } catch (err: any) {
+      return res.status(500).send(err.toString());
+    }
   }
 
   static async updateMenuList(req: Request, res: Response) {
-    res.status(400).end();
+    const token = req.headers.authorization;
+    if (token === undefined) {
+      return res.status(400).end();
+    }
+    const { uid } = await this.getUId(token);
+
+    const validateReq = validateParamWithData<{
+      params: { menuListId: string };
+      body: { title?: string; desc?: string; menu?: IBeverage[] };
+    }>(
+      {
+        params: req.query,
+        body: req.body,
+      },
+      JSCUpdateMenuList,
+    );
+    if (validateReq.result === false) {
+      return res.status(400).send({
+        text: validateReq.errorMessage,
+      });
+    }
+
+    try {
+      const result = MenuListModel.update({
+        ownerId: uid,
+        id: validateReq.data.params.menuListId,
+        title: validateReq.data.body.title,
+        desc: validateReq.data.body.desc,
+        menu: validateReq.data.body.menu,
+      });
+      return res.json(result);
+    } catch (err: any) {
+      return res.status(500).send(err.toString());
+    }
+  }
+
+  static async getUId(token: string) {
+    const decodedToken = await FirebaseAdmin.getInstance().Auth.verifyIdToken(token);
+    return { uid: decodedToken.uid };
   }
 }
